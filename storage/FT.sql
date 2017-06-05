880cca3fdf2452aa7095911b69651f5b0df236e5
francisco-PC\francisco <xnofub@gmail.com>
2017-06-05 02:31:58 -0400

    DB queris CSV

---
+++ storage/FT.sql
@@ -0,0 +1,19 @@
+SELECT * FROM `cartas`
+WHERE `CRT_NOMBRE` LIKE '%Dark r%'
+
+
+UPDATE cartas
+SET `CRT_IMAGEN` = CONCAT('http://magiccards.info/scans/en/',LOWER(EDN_COD_INTERNO),'/',CRT_NUMERO_EDICION,'.jpg') 
+
+
+
+INSERT INTO `ediciones` (
+`EDN_NOMBRE`
+,`EDN_COD_INTERNO`
+)
+SELECT CRT_EDICION
+, `EDN_COD_INTERNO`
+FROM cartas 
+WHERE  EDN_COD_INTERNO = 'B'
+GROUP BY CRT_EDICION
+
