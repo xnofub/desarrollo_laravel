4b0ade12396408149a9af9ca8f61994b0a61f626
Francisco Carrasco <fcarrasco@Celmedia.chile>
2017-06-07 19:08:31 -0300

    cambios visuales

--- resources/views/backend/eventosmazos/index.blade.php
+++ resources/views/backend/eventosmazos/index.blade.php
@@ -4,53 +4,44 @@
 
 @include('layouts.flash')
 <div class="row">
-<a class="btn btn-success pull-right btn_ok" data-toggle="modal" data-target="#myModal" href="{!! URL::to('participantes/scoreparticipantes/'.$evento->EVN_ID) !!}">Agregar</a>
-</div>
-
-   
-<div>
-
+    <div class="col-xs-12 col-sm-12 col-lg-12">
+    <a class="btn btn-success btn_ok" data-toggle="modal" data-target="#myModal" href="{!! URL::to('participantes/scoreparticipantes/'.$evento->EVN_ID) !!}">Agregar</a>
+    <br><br>
+    </div>
 </div>
 <div class="row">
     @foreach($participantes as $p)
-        <div class="col-sm-12 col-md-6 col-lg-4">
-            <div class="list-group">
-                <div class="list-group-item">
-                    <div>
-                        <label>Posicion: </label>
-                        {{$p->EVM_POSICION}}
-                        
-                        <label>Nombre Mazo: </label>
-                        {{$p->EVM_NOMBRE_MAZO}}
-                        
-                        <label>Tipo Mazo: </label>
-                        {{$p->ToMazos->MAZ_NOMBRE}}
-                        
-                        <label>Jugador: </label>
-                        {{$p->ToJugadores->JGD_NOMBRE}}
-                        
-                        <label>Evento: </label>
-                        {{$p->ToEventos->EVN_NOMBRE}}
-                                <br>
-                        <label>Editar: </label>
-                        <a class="btn btn-default btn-xs btn-warning edit" data-toggle="modal" data-target="#myModal" title="Editar" href="{{ url('participantes/editscoreparticipantes/' . $p->EVM_ID . '') }}"> Editar</i></a>
-                                <br>
-                        
+        <div class="col-sm-4">
+            <div class="panel panel-default"">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{$p->EVM_POSICION}}  {{$p->ToMazos->MAZ_NOMBRE}}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="col-lg-12"> 
+                        <p>{{$p->EVM_NOMBRE_MAZO}}</p>
+                        <p>{{$p->ToJugadores->JGD_NOMBRE}}</p>
+                        <p>{{$p->ToEventos->EVN_NOMBRE}}</p>
+                    </div>
+                    
+                    <div class="col-xs-4  col-sm-4 col-lg-4">
+                        <a class="btn btn-default  btn-warning edit" data-toggle="modal" data-target="#myModal" title="Editar" href="{{ url('participantes/editscoreparticipantes/' . $p->EVM_ID . '') }}"> <span class="glyphicon glyphicon-pencil"></span></a> 
+                    </div>
+                    <div class="col-xs-4  col-sm-4 col-lg-4">
                         {!! Form::model($p, array('route' => array('participantes.destroy', $p->EVM_ID), 'method'=>'DELETE', 'class' => 'form-horizontal editar', 'role'=>'form')) !!}
                             {!! Form::hidden('EVN_ID',$evento->EVN_ID, ['class' => 'form-control','readonly'=>'readonly']) !!}
                             {!! Form::hidden('EVM_ID',$p->EVM_ID, ['class' => 'form-control','readonly'=>'readonly']) !!}
-                            <label>Eliminar: </label>
-                            <button type="submit" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-minus-sign"></span></button>
+                            <button type="submit" class="btn  btn-danger"><span class="glyphicon glyphicon-minus-sign"></span></button>
                         {!! Form::close() !!}
-                        <label>Lista: </label>
-                        <a class="btn btn-default btn-xs btn-primary edit"  title="Editar" href="{{ url('lista/' . $p->EVM_ID . '') }}"> <span class="glyphicon glyphicon-plus-sign"></span></a>
-                                
-                        
-                        
+                    </div>
+                    <div class="col-xs-4  col-sm-4 col-lg-4">
+                        <a class="btn btn-default  btn-primary edit"  title="Agregar" href="{{ url('lista/' . $p->EVM_ID . '') }}"> <span class="glyphicon glyphicon-plus-sign"></span></a>
                     </div>
                 </div>
             </div>
         </div>
+    
+    
+        
     @endforeach
 </div>
 <div id="myModal" class="modal fade" role="dialog">
