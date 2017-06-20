4d2a34039099603bb1d36809ff4b9b37bd999bc4
Francisco Carrasco <fcarrasco@Celmedia.chile>
2017-06-20 14:10:28 -0300

    Analitics

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -76,6 +76,14 @@
             <div class="container">
                 <p class="text-muted"><b>EQUIPO FETCHTUTOR</b></p>
             </div>
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                ga('create', 'UA-101354326-1', 'auto');
+                ga('send', 'pageview');
+            </script>
         </footer>
         <!-- Scripts -->
         {!! Html::script('assets/js/jquery.min.js') !!}
