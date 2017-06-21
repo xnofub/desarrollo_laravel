869f21925edab08bb642b7813d898598b29d6b5f
francisco-PC\francisco <xnofub@gmail.com>
2017-06-20 22:44:58 -0400

    no message

---
+++ resources/views/front/email/send.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.front')
+@section('title', 'Contacto')
+@section('content')
+<div class="container">
+    <div class="row">
+        
+        <div class="col-lg-12 col-sm-12 col-xs-12">
+            
+            
+            
+        </div>
+        
+    </div>
+</div>
+@endsection
+@section('js')
+<script>
+    $(document).ready(function () {
+        
+    });
+    
+</script>
+@endsection
